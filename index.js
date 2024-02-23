526d2626b234f49245ce629a709154ae76d2898b
Alexander Zaloga <138583718+agarin6@users.noreply.github.com>
2024-02-23 14:42:49 +0300

    init: create express app

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import multer from 'multer';
+import cors from 'cors';
+
+mongoose
+    .connect('mongodb+srv://admin:He12345678@cluster0.k6wg7rw.mongodb.net/ComputerSchool?retryWrites=true&w=majority')
+    .then(() => console.log('DB OK'))
+    .catch((err) => console.log('DB ERROR', err));
+
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+//media upload pathes
+app.post('/upload',  upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
+
+// //auth
+// app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+// app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+// app.get('/auth/me', allRolesAuth, UserController.getMe);
+// app.get('/user/:userId', allRolesAuth, UserController.getUserById);
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Server OK');
+});
