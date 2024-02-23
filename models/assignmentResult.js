9708631998c949e762f9e94b4aaddeee949fbd5a
Alexander Zaloga <138583718+agarin6@users.noreply.github.com>
2024-02-23 15:30:21 +0300

    feat: add assignmnet result

---
+++ models/assignmentResult.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const AssignmentResultSchema = new mongoose.Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        gitHubLink: {
+            type: String,
+            required: true,
+        },
+        score: {
+            type: Number,
+            required: true,
+        }
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export default mongoose.model('AssignmnetResult', AssignmentResultSchema);
