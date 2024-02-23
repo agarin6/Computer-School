cd5f55cea56b752130bec442bf9e8b7264f03b83
Alexander Zaloga <138583718+agarin6@users.noreply.github.com>
2024-02-23 15:29:58 +0300

    feat: add group model

---
+++ models/group.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const GroupSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        course: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Course',
+            required: true,
+        },
+        students: [
+            {
+                type: mongoose.Schema.Types.ObjectId, 
+                ref: 'User',
+            }
+        ],
+        shedule: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Shedule',
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export default mongoose.model('Groupe', GroupSchema);
