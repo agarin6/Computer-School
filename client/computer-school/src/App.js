08dbaf7d15ffb2ece360e67f9564063b83d89596
Alexander Zaloga <138583718+agarin6@users.noreply.github.com>
2024-02-26 23:44:42 +0300

    feat: add register and login

--- client/computer-school/src/App.js
+++ client/computer-school/src/App.js
@@ -6,6 +6,11 @@ import { fetchAuthMe, selectIsAuth } from './redux/slices/auth';
 import { CssBaseline, Box } from '@mui/material';
 import './App.css';
 
+import LoginPage from './components/login';
+import RegistrationPage from './components/register';
+import Header from './components/header';
+import Footer from './components/footer';
+
 function App() {
   const dispatch = useDispatch();
   const isAuth = useSelector(selectIsAuth);
@@ -17,8 +22,21 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className="App">
-      
+    <div className="App" style={{ overflowX: 'hidden' }}>
+      <CssBaseline />
+      <Box display="flex" flexDirection="column" minHeight="100vh">
+        <Header />
+        <Box component="main" flexGrow={1} sx={{ width: '100%' }}>
+          <Routes>
+            {/* <Route path='/' element={<HomePage></HomePage>} /> */}
+            {!isAuth && <Route path='/registration' element={<RegistrationPage />} />}
+            {!isAuth && <Route path='/login' element={<LoginPage />} />}
+            {isAuth && <Route path="*" element={<Navigate to="/" />} />}
+            {!isAuth && <Route path="*" element={<Navigate to="/login" />} />}
+          </Routes>
+        </Box>
+        <Footer></Footer>
+      </Box>
     </div>
   );
 }
